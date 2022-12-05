134e747eb653430ab9a86c9d64d4c2e243ff16c8
Jonatan <jonatan.vergara.27@gmail.com>
2022-12-05 17:08:09 -0500

    commit inicial semestral

--- semestra_front/script.js
+++ semestra_front/script.js
@@ -1,17 +1,20 @@
 const piramide = [];
 const ruta = [];
+let uwu = null;
 
 for(let x = 0;  10 > x;){
     piramide[x] =  [];
     for (let y = 0; x >= y;){  
             piramide[x][y] = y + 1;
             if(piramide[x][y] > piramide[x][y-1]){
-                ruta[x] = piramide[x][y]
-                console.log( ruta[x] );
+                uwu = piramide[x][y]
+               
             }
             document.write( piramide[x][y] );
             y++;
     }
     document.write("<br/>");
+    ruta[x] = uwu
+    console.log( ruta[x] );
     x++; 
 }  
